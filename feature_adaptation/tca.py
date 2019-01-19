f8d46bd5dc0ed3558cbf59b4075a3dfd4d55d9cf
Shuo Zhou <szhou20@sheffield.ac.uk>
2019-01-19 16:42:49 +0000

    update sstca

--- feature_adaptation/tca.py
+++ feature_adaptation/tca.py
@@ -140,3 +140,78 @@ class SSTCA(BaseEstimator, TransformerMixin):
         self.kwargs = kwargs
         self.kernel = kernel
         self.lambda_ = lambda_    
+
+    def fit(self, Xs, Xt, ys = None, yt = None):
+        '''
+        Parameters:
+            Xs: Source domain data, array-like, shape (n_samples, n_feautres)
+            Xt: Target domain data, array-like, shape (n_samples, n_feautres)
+        '''
+        self.ns = Xs.shape[0]
+        self.nt = Xt.shape[0]
+        n = self.ns + self.nt
+        X = np.vstack((Xs, Xt))
+        L = get_L(self.ns, self.nt)
+        L[np.isnan(L)] = 0
+        K = get_kernel(X, kernel = self.kernel, **self.kwargs)
+        K[np.isnan(K)] = 0
+
+        if ys is not None and yt is not None:
+            y = np.concatenate((ys, yt))    
+            y = y.reshape((n,1))
+            Kyy = np.dot(y, y.T)
+        #obj = np.trace(np.dot(K,L))
+
+        H = np.eye(n) - 1. / n * np.ones((n, n))
+        
+        obj = np.dot(np.dot(K, L), K.T) + self.lambda_ * np.eye(n)
+        if ys is not None and yt is not None:
+            hsic = np.matmul(np.matmul(H, Kyy), H)
+            st = np.dot(np.dot(K, (H+hsic)), K.T)
+        else:
+            st = np.dot(np.dot(K, H), K.T)
+        eig_vals, eig_vecs = scipy.linalg.eig(obj, st)
+        
+#        ev_abs = np.array(list(map(lambda item: np.abs(item), eig_vals)))
+#        idx_sorted = np.argsort(ev_abs)
+        idx_sorted = eig_vals.argsort()
+
+       
+        self.eig_vals = eig_vals[idx_sorted]
+        self.U = eig_vecs[:, idx_sorted]
+        self.U = np.asarray(self.U, dtype = np.float)
+#        self.components_ = np.dot(X.T, U)
+#        self.components_ = self.components_.T
+
+        self.Xs = Xs
+        self.Xt = Xt
+        return self
+
+    def transform(self, X):
+        '''
+        Parameters:
+            X: array-like, shape (n_samples, n_feautres)
+        Return:
+            tranformed data
+        '''
+        check_is_fitted(self, 'Xs')
+        check_is_fitted(self, 'Xt')
+        X_fit = np.vstack((self.Xs, self.Xt))
+        K = get_kernel(X, X_fit, kernel = self.kernel, **self.kwargs)
+        U_ = self.U[:,:self.n_components]
+        X_transformed = np.dot(K, U_)
+        return X_transformed
+
+
+    def fit_transform(self, Xs, Xt, ys=None, yt=None):
+        '''
+        Parameters:
+            Xs: Source domain data, array-like, shape (n_samples, n_feautres)
+            Xt: Target domain data, array-like, shape (n_samples, n_feautres)
+        Return:
+            tranformed Xs_transformed, Xt_transformed
+        '''
+        self.fit(Xs, Xt, ys, yt)
+        Xs_transformed = self.transform(Xs)
+        Xt_transformed = self.transform(Xt)
+        return Xs_transformed, Xt_transformed
